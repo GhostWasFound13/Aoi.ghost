fd5c1b8100eee871d60c0287fd32201df7f128b5
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-12-01 15:09:49 +0700

    Create Warn.js

---
+++ src/Handler/Warn.js
@@ -0,0 +1,9 @@
+dule.exports = {
+    fnWarn: (msg) => {
+        try {
+            console.log(`\x1b[33maoi.ghostWarning:\x1b[97m ${msg}`);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+}
